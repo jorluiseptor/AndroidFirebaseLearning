985c76a540a8aa4117d3d2a019dd7d4575a1c723
Jorge Sepulveda <jorluiseptor@gmail.com>
2016-06-11 12:57:42 -0400

    First push.

---
+++ app/src/main/java/com/jorgesepulveda/firebasetest/Constants.java
@@ -0,0 +1,9 @@
+package com.jorgesepulveda.firebasetest;
+
+/**
+ * Created by Jorge on 6/11/2016.
+ */
+
+public class Constants {
+    public static final String FIREBASE_URL = "project-3866287992935656530.firebaseio.com";
+}
